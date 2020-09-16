9e4b624f28a193b9b28302f37c6cc0a52d8c8068
ttzztztz <ttzztztz@outlook.com>
2020-09-16 11:37:58 +0800

    decouple: util module

--- utils/files/file.go
+++ utils/files/file.go
@@ -1,13 +1,7 @@
 package files
 
 import (
-	"io/ioutil"
 	"os"
-	"path/filepath"
-)
-
-var (
-	MagicBytes = []byte{'\x7F', 'E', 'L', 'F'}
 )
 
 func Exists(path string) bool {
@@ -19,41 +13,4 @@ func Exists(path string) bool {
 		return false
 	}
 	return true
-}
-
-func TouchFile(path string) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-
-	defer func() {
-		_ = f.Close()
-	}()
-
-	_, err = f.Write([]byte(""))
-	return err
-}
-
-func TouchFileWithMagic(path string) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-
-	defer func() {
-		_ = f.Close()
-	}()
-
-	_, err = f.Write(MagicBytes)
-	return err
-}
-
-func ReadFileBytes(absPath string) ([]byte, error) {
-	path, err := filepath.Abs(absPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return ioutil.ReadFile(path)
 }
--- utils/files/path.go
+++ utils/files/path.go
@@ -36,43 +36,6 @@ func CodeGenerateFileNameWithMkdir(uid uint32) (string, error) {
 	return fmt.Sprintf("%s/%d_%d", t.Format("2006/01/02"), uid, t.UnixNano()), nil
 }
 
-func SubmissionBaseDir() (string, error) {
-	return filepath.Abs("./files/submission/")
-}
-
-func SubmissionGenerateDirWithMkdir(sid uint32) (string, error) {
-	t := time.Now()
-
-	path, err := SubmissionBaseDir()
-	if err != nil {
-		return "", err
-	}
-	path, err = filepath.Abs(fmt.Sprintf("%s/%s/%d", path, t.Format("2006/01/02"), sid))
-	if err != nil {
-		return "", err
-	}
-
-	if !Exists(path) {
-		if err := os.MkdirAll(path, 0755); err != nil {
-			return "", err
-		}
-	}
-
-	return path, nil
-}
-
-func JudgeGenerateOutputDirWithMkdir(baseDir string) (string, error) {
-	path := baseDir + "/output"
-
-	if !Exists(path) {
-		if err := os.MkdirAll(path, 0755); err != nil {
-			return "", err
-		}
-	}
-
-	return path, nil
-}
-
 func StorageFilePath() (string, error) {
 	return filepath.Abs("./files/storage.json")
 }
@@ -95,16 +58,8 @@ func JudgeDirPathWithMkdir(tid uint32, version string) (string, error) {
 	return path, nil
 }
 
-func JudgeCaseDir(tid uint32, version string) (string, error) {
-	return filepath.Abs(fmt.Sprintf("./files/judge/%d/%s", tid, version))
-}
-
 func JudgeFilePath(tid uint32, version, caseId, caseType string) (string, error) {
 	return filepath.Abs(fmt.Sprintf("./files/judge/%d/%s/%s.%s", tid, version, caseId, caseType))
-}
-
-func DockerHostConfigBinds(source, target string) string {
-	return fmt.Sprintf("%s:%s", source, target)
 }
 
 func CertFilePath(fileName string) (string, error) {
